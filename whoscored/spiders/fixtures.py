55e1a4a542ac79c63f28df254b7e6aaafc8f90ab
Johan Wicklund <wicklund@hotmail.com>
2015-12-17 10:31:00 +0100

    Updates to fixture spider

--- whoscored/spiders/fixtures.py
+++ whoscored/spiders/fixtures.py
@@ -22,14 +22,10 @@ class FixtureSpider(Spider):
 
     def parse(self, response):
         data = response.xpath('//script[contains(., "Model-Last-Mode")]/text()').re_first(r"'Model-Last-Mode': '(.*?)' }")
-        try:
-            model_last_mode = data
-        except ValueError:
-            self.logger.warning("Model-Last-Mode not found: ")
-            model_last_mode = None
+        model_last_mode = data
 
         request = Request(
-            url="http://www.whoscored.com/tournamentsfeed/{}/Fixtures?={}&isAggregate=false",
+            url="http://www.whoscored.com/tournamentsfeed/{}/Fixtures?d={}&isAggregate=false".format(self.arg_list[3], self.arg_list[4]),
             headers={'X-Requested-With': 'XMLHttpRequest', 'Host': 'www.whoscored.com', 'Model-Last-Mode': model_last_mode},
             callback=self.parse_fixtures
         )
@@ -37,16 +33,9 @@ class FixtureSpider(Spider):
         return request
 
     def parse_fixtures(self, response):
-        list = response.xpath('//script[contains(., "DataStore.prime(\'stagefixtures\'")]/text()').extract()
-
-        if len(list) == 0:
-            self.logger.warning("No matchStats found.")
-            return
-
-        data = re.search(r"calendar\.parameter\(\)\), ([\w\W]*?)\);", list[0])
-
+        data = response.body
         if data:
-            data = re.sub(r',,', r',null,', data.group(1))
+            data = re.sub(r',,', r',null,', data)
             data = re.sub(r',,', r',null,', data)
             data = re.sub(r'"', r'\"', data)
             data = re.sub(r"\\'", r"'", data)
@@ -58,24 +47,25 @@ class FixtureSpider(Spider):
 
         for record in fixtures:
             ret = Fixture()
-            ret['Id'] = record[0];
-            ret['Status'] = record[1];
-            ret['StartDate'] = record[2];
-            ret['StartTime'] = record[3];
-            ret['HomeTeamId'] = record[4];
-            ret['HomeTeamName'] = record[5];
-            ret['HomeRCards'] = record[6];
-            ret['AwayTeamId'] = record[7];
-            ret['AwayTeamName'] = record[8];
-            ret['AwayRCards'] = record[9];
-            ret['Score'] = record[10];
-            ret['HTScore'] = record[11];
-            ret['HasIncidents'] = record[12];
-            ret['HasPreview'] = record[13];
-            ret['Elapsed'] = record[14];
-            ret['Result'] = record[15];
-            ret['IsInternational'] = record[16];
-            ret['IsOpta'] = record[19] or record[17];
+            ret['stage'] = self.arg_list[3]
+            ret['id'] = record[0];
+            ret['status'] = record[1];
+            ret['start_date'] = record[2];
+            ret['start_time'] = record[3];
+            ret['home_team_id'] = record[4];
+            ret['home_team_name'] = record[5];
+            ret['home_red_cards'] = record[6];
+            ret['away_team_id'] = record[7];
+            ret['away_team_name'] = record[8];
+            ret['away_red_cards'] = record[9];
+            ret['score'] = record[10];
+            ret['ht_score'] = record[11];
+            ret['has_incidents'] = record[12];
+            ret['has_preview'] = record[13];
+            ret['elapsed'] = record[14];
+            ret['result'] = record[15];
+            ret['is_international'] = record[16];
+            ret['is_opta'] = record[19] or record[17];
 
             yield ret
 
